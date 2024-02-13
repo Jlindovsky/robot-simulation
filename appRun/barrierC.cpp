0541acd8a05c255102292d39f31d3cebdb6f41f6
Jlindovsky <j.lindovsky@icloud.com>
2024-02-13 17:15:12 +0100

    sdf

--- appRun/barrierC.cpp
+++ appRun/barrierC.cpp
@@ -1,8 +1,11 @@
 #include "barrierC.h"
 
 barrierC::barrierC(qreal x, qreal y, qreal w, qreal h, QGraphicsRectItem *parent)
-    : QGraphicsRectItem(x, y, w, h, parent)
+    : QGraphicsRectItem(0, 0, w, h, parent)
 {
+
+    this->setPos(mapToParent(x, y));
+    // hitbox = new Hitbox(this);
     // Additional initialization code specific to barrierC if needed
 }
-barrierC::~barrierC(){}
+barrierC::~barrierC() {}
--- appRun/window.cpp
+++ appRun/window.cpp
@@ -44,52 +44,56 @@ void window::editWindowSignal()
     editScene->setSceneRect(0, 0, 1024, 768);
     setScene(editScene);
 
-    QGraphicsRectItem *playground = new QGraphicsRectItem(70, 100, 500, 500);
-    playground->setPos(70, 100);
+    QGraphicsRectItem *playground = new QGraphicsRectItem(0, 0, 500, 500);
+    playground->setPos(170, 100);
     QPen pen;
     pen.setColor(Qt::white); // Set the border color (replace with your desired color)
     pen.setWidth(4);         // Set the border width (replace with your desired width)
     playground->setPen(pen);
     editScene->addItem(playground);
 
-    barrierC *edgeTop = new barrierC(playground->x(), playground->y(), 500, 1, playground);
-    edgeTop->setPos(0, 0);
+    QGraphicsRectItem *panel = new QGraphicsRectItem(0, 0, 60, 768);
+    panel->setPos(0, 0);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::white);
+    panel->setBrush(brush);
+    editScene->addItem(panel);
+
+    barrierC *edgeTop = new barrierC(0, 0, 500, 1, playground);
     pen.setColor(Qt::cyan);
     edgeTop->setPen(pen);
     editScene->addItem(edgeTop);
 
-    barrierC *edgeBottom = new barrierC(playground->x(), playground->y(), 500, 1, playground);
-    edgeBottom->setPos(0, 500);
+    barrierC *edgeBottom = new barrierC(0, 500, 500, 1, playground);
     edgeBottom->setPen(pen);
     editScene->addItem(edgeBottom);
 
-    barrierC *edgeLeft = new barrierC(playground->x(), playground->y(), 1, 500, playground);
-    edgeLeft->setPos(0, 0);
+    barrierC *edgeLeft = new barrierC(0, 0, 1, 500, playground);
     pen.setColor(Qt::red);
     edgeLeft->setPen(pen);
     editScene->addItem(edgeLeft);
 
-    barrierC *edgeRight = new barrierC(playground->x(), playground->y(), 1, 500, playground);
-    edgeRight->setPos(500, 0);
+    barrierC *edgeRight = new barrierC(500, 0, 1, 500, playground);
     edgeRight->setPen(pen);
     editScene->addItem(edgeRight);
 
-    barrierC *bar1 = new barrierC(playground->x(), playground->y(), 100, 100, playground);
-    bar1->setPos(250, 250);
+    barrierC *bar1 = new barrierC(250, 250, 100, 100, playground);
+
     pen.setColor(Qt::magenta);
     bar1->setPen(pen);
     editScene->addItem(bar1);
 
-    barrierC *bar2 = new barrierC(playground->x(), playground->y(), 100, 100, playground);
-    bar2->setPos(50, 50);
+    barrierC *bar2 = new barrierC(-50, -50, 100, 100, playground);
+
     pen.setColor(Qt::magenta);
     bar2->setPen(pen);
     editScene->addItem(bar2);
 
     Robot *rob1 = new Robot(0, 0, 50, playground);
-    QBrush brush(Qt::blue);
-    brush.setColor(Qt::darkMagenta);
-    rob1->setBrush(brush);
+    QBrush brushRob(Qt::blue);
+    brushRob.setColor(Qt::darkMagenta);
+    rob1->setBrush(brushRob);
 
     QTimer *timer = new QTimer();
     QObject::connect(timer, SIGNAL(timeout()), rob1, SLOT(move()));
